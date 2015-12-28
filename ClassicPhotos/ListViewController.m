b6d1bac4f8b5bab19836eea23e0d5a5b233f4ab5
Brusnikin <brusnikinapps@gmail.com>
2015-12-29 01:42:14 +0300

    first commit

--- ClassicPhotos/ListViewController.m
+++ ClassicPhotos/ListViewController.m
@@ -13,11 +13,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
     self.title = @"Classic Photos";
     self.tableView.rowHeight = 80;
 //    self.tableView.dataSource = self;
